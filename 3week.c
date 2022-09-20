e1266151b6ebce0d82d1e775c868c373a6e1ac3f
AmondeBong <oms1218ju@gmail.com>
2022-09-20 14:04:07 +0900

    3week homework

---
+++ 3week.c
@@ -0,0 +1,19 @@
+#define F_CPU 16000000L
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRB = 0xFF; //출력으로 설ㅓ
+    while (1)
+    {
+        PORTB = 0xA0;    // 1011 0000
+        _delay_ms(2000); // 2초로 설정
+        PORTB = 0x28;    // 0010 1000
+        _delay_ms(2000); // 2초로 설정
+        PORTB = 0x0A;    // 0000 1010
+        _delay_ms(2000); // 2초로 설정
+        PORTB = 0x82;    // 1000 0010
+        _delay_ms(2000); // 2초로 설정
+    }
+}
